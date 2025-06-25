45a5afe48b5dc52fd17ff10ab6d2597ef706b307
charyna-chab <charyna.chab@student.passerellesnumeriques.org>
2025-06-25 21:04:14 +0700

    category and member are work well

--- src/api/category.js
+++ src/api/category.js
@@ -1,2 +1,5 @@
 import apiServices from "@/plugins/axios";
 export const index = () => apiServices.get('/category')
+export const store = (data) => apiServices.post('/category', data);
+export const update = (data) => apiServices.put(`/category/${data.id}`, data);
+export const destroy = (id) => apiServices.delete(`/category/${id}`);
