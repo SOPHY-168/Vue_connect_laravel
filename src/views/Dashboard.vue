2fee91b8f21f46ad170b5aa480e50e3262b9cc00
charyna-chab <charyna.chab@student.passerellesnumeriques.org>
2025-06-25 20:28:19 +0700

    count total author,borrowing,category,member, and books store in dashboard cart

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -23,12 +23,54 @@
 </template>
 
 <script setup>
+import { ref, onMounted, watch } from 'vue'
+import { index as fetchBooks } from '@/api/book'
+import { index as fetchMembers } from '@/api/member'
+import { index as fetchAuthors } from '@/api/author'
+import { index as fetchCategory } from '@/api/category'
+import { index as fetchBorrowing } from '@/api/borrowing'
+const books = ref([])
+const members = ref([])
+const authors = ref([])
+const category = ref([])
+const borrowing = ref([])
 
-const dashboardItems = [
-  { label: 'Books', count: 120, icon: '📘', bg: 'bg-red-400' },
-  { label: 'Members', count: 80, icon: '👥', bg: 'bg-green-500' },
-  { label: 'Categories', count: 10, icon: '📂', bg: 'bg-yellow-400' },
-  { label: 'Authors', count: 25, icon: '✍️', bg: 'bg-blue-500' },
-  { label: 'Borrowings', count: 45, icon: '📄', bg: 'bg-indigo-500' },
-];
+onMounted(async () => {
+  try {
+    // book
+    const booksRes = await fetchBooks()
+    books.value = Array.isArray(booksRes.data) ? booksRes.data : booksRes.data.data
+    // member
+    const membersRes = await fetchMembers()
+    members.value = Array.isArray(membersRes.data) ? membersRes.data : membersRes.data.data
+    // category
+    const categoryRes = await fetchCategory()
+    category.value = Array.isArray(categoryRes.data) ? categoryRes.data : categoryRes.data.data
+    // authors
+    const authorsRes = await fetchAuthors()
+    authors.value = Array.isArray(authorsRes.data) ? authorsRes.data : authorsRes.data.data
+    // borrowing
+    const borrowRes = await fetchBorrowing()
+    borrowing.value = Array.isArray(borrowRes.data) ? borrowRes.data : borrowRes.data.data
+  } catch (error) {
+    console.error('Failed to fetch books for dashboard:', error)
+  }
+})
+
+const dashboardItems = ref([
+  { label: 'Books', count: 0, icon: '📘', bg: 'bg-red-400' },
+  { label: 'Members', count: 0, icon: '👥', bg: 'bg-green-500' },
+  { label: 'Categories', count: 0, icon: '📂', bg: 'bg-yellow-400' },
+  { label: 'Authors', count: 0, icon: '✍️', bg: 'bg-blue-500' },
+  { label: 'Borrowings', count: 0, icon: '📄', bg: 'bg-indigo-500' },
+])
+
+// Update book count when books are fetched
+watch([books, members, authors,category,borrowing], () => {
+  dashboardItems.value[0].count = books.value.length
+  dashboardItems.value[1].count = members.value.length // members
+  dashboardItems.value[2].count = category.value.length // category
+  dashboardItems.value[3].count = authors.value.length // authors
+  dashboardItems.value[4].count = borrowing.value.length // borrowing
+})
 </script>
